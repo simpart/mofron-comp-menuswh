d8f2f4e6f3b41fc56031fa59a21c4d801788d98c
simpart <simpart>
2018-07-02 08:48:04 +0000

    rename, add focus

--- index.js
+++ index.js
@@ -1,128 +1,55 @@
 /**
- * @file   mofron-comp-{@comp-name}/index.js
+ * @file   mofron-comp-menuswitch/index.js
  * @author simpart
  */
 let mf = require('mofron');
+let Switch = require('mofron-comp-visiswh');
+let Focus = require('mofron-event-focus');
+
 /**
  * @class mofron.comp.MenuSwitch
- * @brief contents menu component for mofron
+ * @brief menu switch component for mofron
  */
-mf.comp.MenuSwitch = class extends mf.Component {
+mf.comp.MenuSwh = class extends Switch {
     
-    constructor (po, idx) {
+    constructor (po, p2) {
         try {
             super();
-            this.name('MenuSwitch');
-            this.prmOpt(po, idx);
+            this.name('MenuSwh');
+            this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    /**
-     * initialize dom contents
-     * 
-     * @param prm : (Date object) display date
-     */
-    initDomConts (prm, idx) {
+    switch (prm) {
         try {
+            let evt = (flg, swh, mns) => {
+                try {
+                    mns.status(flg);
+                } catch (e) {
+                    console.error(e.stack);
+                    throw e;
+                }
+            };
             if (undefined !== prm) {
-                if (undefined !== idx) {
-                    prm.selectIdx(idx);
-                }
-                this.menu(prm);
-                this.switchConts(prm, this);
+                prm.addEvent(new Focus(evt, this));
             }
+            return super.switch(prm);
         } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    menu (val) {
-        try {
-            if (undefined === val) {
-                /* getter */
-                return (undefined === this.m_menu) ? null : this.m_menu;
-            }
-            /* setter */
-            if (true !== mf.func.isInclude(val, 'Menu')) {
-                throw new Error('invalid parameter');
-            }
-            val.selectEvt(this.switchConts, this);
-            /* set select index */
-            if (null === val.selectIdx()) {
-                val.selectIdx(0);
-            }
-            /* display initial contents */
-            this.switchConts(val, this);
-            this.m_menu = val;
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    addChild (chd, idx) {}
-    
-    contents (cnt) {
-        try {
-            if (undefined === cnt) {
-                /* getter */
-                return (undefined === this.m_mnu_conts) ? null : this.m_mnu_conts;
-            }
-            /* setter */
-            if (('object' === typeof cnt) && ('undefined' !== typeof cnt[0])) {
-                for (let cidx in cnt) {
-                    this.contents(cnt[cidx]);
-                }
-            } else {
-                if (true !== mf.func.isInclude(cnt, 'Component')) {
-                    throw new Error('invalid parameter');
-                }
-                if (undefined === this.m_mnu_conts) {
-                    this.m_mnu_conts = new Array();
-                }
-                this.m_mnu_conts.push(cnt);
-            }
-            this.switchConts(this.menu(), this);
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    switchConts (mnu, prm) {
-        try {
-            if (null === mnu) {
-                return;
-            }
-            let conts  = prm.contents();
-            let selidx = mnu.selectIdx();
-            if ( (null === conts)     ||
-                 (0 === conts.length) ||
-                 (conts.length <= selidx) ) {
-                return;
-            }
-            for (let cidx in conts) {
-                conts[cidx].visible((selidx == cidx) ? true : false);
-            }
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    themeConts () {
-        try {
             
+        }
+    }
+    
+    menu (prm) {
+        try {
+            return this.swhTarget(prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
 }
-module.exports = mofron.comp.MenuSwitch;
+module.exports = mofron.comp.MenuSwh;
 /* end of file */
